dc3a04e0028af7fa66cb7c966a31db07b7798623
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-04-28 11:28:43 -0400

    made js functional

--- js/palindromechecker.js
+++ js/palindromechecker.js
@@ -1,3 +1,28 @@
+const form = document.querySelector('form');
+const submittedWord = document.querySelector('#submitted-word');
+const result = document.querySelector('#result');
+
+form.addEventListener('submit', (e) => {
+  // GET INPUT
+  let input = submittedWord.value;
+
+  // RUN INPUT THROUGH PALINDROME CHECKER
+  let palindromed = palindrome(input);
+
+  // DISPLAY RESULTS
+  result.innerHTML = `Result: ${
+    palindromed ? "It's a PALINDROME" : 'TRY AGAIN'
+  }`;
+
+  // STOP REDIRECT
+  e.preventDefault();
+
+  input.value = '';
+});
+
+// =====================================
+// PALINDROME CHECKER FUNCTION
+// =====================================
 function palindrome(str) {
   // REMOVE PUNCTUATION
   let punctuationRemoved = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
@@ -20,6 +45,6 @@ function palindrome(str) {
 }
 
 // TEST FUNCTION
-console.log(palindrome('eye'));
-console.log('=======================================');
-console.log(palindrome('A man, a plan, a canal. Panama'));
+// console.log(palindrome('eye'));
+// console.log('=======================================');
+// console.log(palindrome('A man, a plan, a canal. Panama'));
